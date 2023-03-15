3254f7f11a6fe5c29c56d05629a6c930a3701f66
Митев Дарио <dario.mitev@students.finki.ukim.mk>
2023-03-15 20:24:16 +0100

    Fix Null User Bug

--- app/src/components/Pages/MyFavoriteBooks.jsx
+++ app/src/components/Pages/MyFavoriteBooks.jsx
@@ -16,11 +16,11 @@ const MyFavoriteBooks = () => {
   return (
     <div className="container py-5 page">
       <h1>My Favorite Books</h1>
-      {user.favBooks.length === 0 ? (
+      {user && user.favBooks.length === 0 ? (
         <p className="text-muted">You don't have any favorites yet.</p>
       ) : (
         <ol>
-          {user.favBooks.map((book, index) => {
+          {user && user.favBooks.map((book, index) => {
             return <li key={index}>{book}</li>;
           })}
         </ol>
